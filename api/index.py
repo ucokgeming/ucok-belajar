f919799e4c06bb19dea80db1948cfb253971af91
ucokgeming1 <lendingatuh@example.com>
2025-01-19 11:09:27 +0700

    opik caley

--- api/index.py
+++ api/index.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'Hello, opik!'
 
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/ucok')
+def ucok():
+    return 'ucok'
